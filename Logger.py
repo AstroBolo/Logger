bcf61762a56b0ca9d28f8aaba44b6ed637df9b00
DedSec <73861354+AstroBolo@users.noreply.github.com>
2021-01-02 19:15:28 -0500

    Add files via upload

---
+++ Logger.py
@@ -0,0 +1,29 @@
+import socket
+import os
+import requests
+import sys
+import urllib
+import time
+from datetime import datetime
+from urllib import request as urlrequests
+
+def logger():
+    s = socket.socket()
+    windows_ip = input("Enter address: 192.168.86.89")
+    try:
+        host = windows_ip
+        port = 80
+        s.bind((host, 80))
+    except:
+        print("address is in use already")
+        print("Use an other address!")
+    while True:
+        try:
+            s.listen(5)
+            conn, address = s.accept()
+            print("[+] IP Logged " + str(address[0]))
+        except:
+            pass
+            print("Exiting...")
+            sys.exit(0)
+logger()
